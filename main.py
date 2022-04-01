6675cdef69bea68786a54e190b691a540c6c8834
yr15TAbiodun-Ade <yr15TAbiodun-Ade@roundhayschool.com>
2022-04-01 12:28:04 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import pygame
+
+
+pygame.init()
+
+PURPLE = (188, 81, 235)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 240)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+PI = 3.141592653
+size = (700, 500)
+button_width = 250
+button_height = 40
+button_colour = BLUE
+text_colour = BLACK
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Computing Project")
+
+class Main_screen:
+    def __init__(self):
+        pass
+
+    def screenevent(self, event):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def update(self):
+        pass
+
+class Help_screen:
+    def __init__(self):
+        pass
+
+    def screenevent(self, event):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def update(self):
+        pass
+
+class Game_screen:
+    def __init__(self):
+        pass
+
+    def screenevent(self, event):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def update(self):
+        pass
+
+class High_scores:
+    def __init__(self):
+        pass
+
+    def screenevent(self, event):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def update(self):
+        pass
